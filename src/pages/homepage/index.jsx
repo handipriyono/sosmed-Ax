e67bdcd6ceb8c577fe17cbed726e8914bc220493
Handi Priyono <handimacbook@gmail.com>
2024-04-19 20:15:06 +0700

    feat: add homepage user index page

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,13 @@
+import UserList from "./components/user-list";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <UserList />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
